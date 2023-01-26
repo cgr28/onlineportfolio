e27f82f64cd1e9b5ddbc31fbf1100b0f6814449f
Colbe Roberson <cgr28@njit.edu>
2023-01-25 23:28:58 -0500

    updated pdf

--- src/sections/Resume.js
+++ src/sections/Resume.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
 import "./Resume.scss"
-import resume from "../imgs/colbe-resume-online-portfolio.jpg"
-import resumePdf from "../imgs/colbe-resume-online-portfolio.pdf"
+import { Document, pdfjs } from 'react-pdf/dist/esm/entry.webpack';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+
+const url = `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = url;
 
 export default function Resume() {
     return (
         <div className="resume container mx-auto mb-5" id="resume">
             <Header title={"Resume"} caption={"My resume.  Ready and available for download."} />
-            <a href={resumePdf} download data-aos="fade-down" data-aos-once="true"><div class="download-button text-center mx-auto mb-3">Download</div></a>
-            <img className="mx-auto resume-img" src={resume} alt="My resume" data-aos="fade-down" data-aos-once="true" />
+            <a href="./public/colbe-resume-portfolio.pdf" download data-aos="fade-down" data-aos-once="true"><div class="download-button text-center mx-auto mb-3">Download</div></a>
+            <Document file="./public/colbe-resume-portfolio.pdf" onLoadError={console.error} />
         </div>
     )
 }
